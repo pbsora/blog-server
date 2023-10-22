d6738c2e3258cff39db094d3875d0633a532bb12
Pedro <128522273+pbsora@users.noreply.github.com>
2023-10-22 14:36:45 -0300

    Update cors
    =

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "*",
+    origin: "https://blog-server-7vkr.onrender.com/",
     credentials: true,
   })
 );
