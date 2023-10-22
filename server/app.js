0d71f9e2819369f92bd79d3cd1a4e5e5a0c054b9
Pedro <128522273+pbsora@users.noreply.github.com>
2023-10-22 14:39:28 -0300

    Update cors

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "https://blog-server-7vkr.onrender.com/",
+    origin: "https://blog-dedbrl7nl-pbsora.vercel.app",
     credentials: true,
   })
 );
