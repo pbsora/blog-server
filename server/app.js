b6e4cff42241985c32a53eb303a47bb1f64635c5
Pedro <128522273+pbsora@users.noreply.github.com>
2023-12-31 15:37:18 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "https://sora-blog-blond.vercel.app/",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
